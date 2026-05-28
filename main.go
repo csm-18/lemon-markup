29c7142a634503e1335fe87f3f89dcb6fec1c5b2
csm-18 <112937168+csm-18@users.noreply.github.com>
2026-05-29 00:31:43 +0530

    cli: version & help commands

--- main.go
+++ main.go
@@ -17,8 +17,9 @@ func main() {
 		} else if args[0] == "build" {
 
 		} else if args[0] == "version" {
-
+			fmt.Println(LEMON_VERSION)
 		} else if args[0] == "help" {
+			fmt.Println(LEMON_HELP)
 
 		} else {
 			fmt.Println(LEMON_CLI_ERROR)
@@ -35,4 +36,11 @@ const LEMON_VERSION = "lemon 0.2.0"
 const LEMON_ABOUT = `Lemon Markup is a simple, strict, compile-time-only static HTML extension language
 For help:
   lemon help`
+
+const LEMON_HELP = `lemon Commands:
+  1. lemon <no args>          -> print about message
+  2. lemon init               -> initialize new lemon project
+  3. lemon build              -> build the project
+  4. lemon version            -> print lemon version
+  5. lemon help               -> print lemon commands list`
 const LEMON_CLI_ERROR = "Error: Unknown command or malformed args passed to lemon!"
